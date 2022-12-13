fdba027529aab211a78fb4cfb944c8f4346fb8b5
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2022-12-12 22:03:37 -0300

    feat: add day 2

---
+++ 2022/day-2-part1.exs
@@ -0,0 +1,39 @@
+defmodule RockPaperScissors do
+  # A, X -> Rock
+  # B, Y -> Paper
+  # C, Z -> Scissors
+
+  # X > C
+  # Y > A
+  # Z > B
+
+  def value_of_our_move("X"), do: 1
+  def value_of_our_move("Y"), do: 2
+  def value_of_our_move("Z"), do: 3
+  def value_of_our_move(_), do: 0
+
+  def value_of_result("Draw"), do: 3
+  def value_of_result("Lose"), do: 0
+  def value_of_result("Win"), do: 6
+
+  def value_of_output("A", "X"), do: value_of_result("Draw")
+  def value_of_output("A", "Y"), do: value_of_result("Win")
+  def value_of_output("A", "Z"), do: value_of_result("Lose")
+  def value_of_output("B", "X"), do: value_of_result("Lose")
+  def value_of_output("B", "Y"), do: value_of_result("Draw")
+  def value_of_output("B", "Z"), do: value_of_result("Win")
+  def value_of_output("C", "X"), do: value_of_result("Win")
+  def value_of_output("C", "Y"), do: value_of_result("Lose")
+  def value_of_output("C", "Z"), do: value_of_result("Draw")
+
+  def calculate_result(round) do
+    [opponent, us] = String.split(round, " ")
+    value_of_output(opponent, us) + value_of_our_move(us)
+  end
+end
+
+File.read("input-day2.txt")
+|> elem(1)
+|> String.split("\n")
+|> Enum.map(&RockPaperScissors.calculate_result/1)
+|> Enum.sum
---
+++ 2022/day-2-part2.exs
@@ -0,0 +1,34 @@
+defmodule RockPaperScissors do
+  # A -> Rock
+  # B -> Paper
+  # C -> Scissors
+  def value_of_our_move("A"), do: 1
+  def value_of_our_move("B"), do: 2
+  def value_of_our_move("C"), do: 3
+  def value_of_our_move(_), do: 0
+
+  # X -> Lose
+  # Y -> Draw
+  # Z -> Win
+  def value_of_result("Draw"), do: 3
+  def value_of_result("Lose"), do: 0
+  def value_of_result("Win"), do: 6
+
+  def calculate_result("A X"), do: value_of_result("Lose") + value_of_our_move("C")
+  def calculate_result("A Y"), do: value_of_result("Draw") + value_of_our_move("A")
+  def calculate_result("A Z"), do: value_of_result("Win") + value_of_our_move("B")
+
+  def calculate_result("B X"), do: value_of_result("Lose") + value_of_our_move("A")
+  def calculate_result("B Y"), do: value_of_result("Draw") + value_of_our_move("B")
+  def calculate_result("B Z"), do: value_of_result("Win") + value_of_our_move("C")
+
+  def calculate_result("C X"), do: value_of_result("Lose") + value_of_our_move("B")
+  def calculate_result("C Y"), do: value_of_result("Draw") + value_of_our_move("C")
+  def calculate_result("C Z"), do: value_of_result("Win") + value_of_our_move("A")
+end
+
+File.read("input-day2.txt")
+|> elem(1)
+|> String.split("\n")
+|> Enum.map(&RockPaperScissors.calculate_result/1)
+|> Enum.sum
