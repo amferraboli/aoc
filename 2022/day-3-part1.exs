73c8c4bd7c010649f3ef99d9a1770a9b52392764
Amanda Ferraboli <amanda.ferraboli@loft.com.br>
2022-12-29 22:21:15 -0300

    feat: add day 3 input and initial part of solution

---
+++ 2022/day-3-part1.exs
@@ -0,0 +1,8 @@
+File.read("input-day-3.txt")
+|> elem(1)
+|> String.split("\n")
+|> Enum.map(fn x -> String.length(x) |> Integer.floor_div(2) end)
+
+# check if all rucksacks have even number of items - sum should be zero
+# |> Enum.map(fn x -> Integer.mod(String.length(x),2) end)
+# |> Enum.sum
