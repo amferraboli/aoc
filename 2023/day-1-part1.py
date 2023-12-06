d79325d0579946d6213ee4bdca1a3e03c825e991
Amanda <amanda.ferraboli@gmail.com>
2023-12-06 18:45:49 +0100

    feat: add comments to solution

--- 2023/day-1-part1.py
+++ 2023/day-1-part1.py
@@ -1,7 +1,9 @@
 total_sum = 0
 
+# open file in 'r' mode (read mode)
 with open("2023/data/day-1-part1.txt", "r") as file:
     for line in file:
+        # for each line, for each character, check if digit
         filtered_digits = [c for c in line if c.isdigit()]
         first_digit = filtered_digits[0]
         last_digit = filtered_digits[-1]
